c8710ccdfbb61ef8da81c1bc5f0d48e0df2dde44
fallguy <harishvijayan2003@gmail.com>
2025-03-25 23:30:43 +0530

    complete winner module

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,34 @@ function App() {
   const [score1, setScore1] = useState(0)
   const [score2, setScore2] = useState(0)
   const [goal,setGoal] = useState(0);
+  const [winner,setWinner] = useState("");
 
   function handleClick1(){
     setScore1(score1+1);
     if(score1+1==goal)
     {
-      alert("Player1 won");
-      reset();
+      setWinner("Player 1");
+
     }
   }
   function handleClick2(){
     setScore2(score2+1);
       if(score2+1==goal)
       {
-        alert("Player2 won");
-        reset();
+        setWinner("Player 2");
       }
   }
   function reset(){
     setScore1(0);
     setScore2(0);
+    setWinner("");
   }
   return (
     <>
       <Title name="Score-Keeper"/>
       <p>{score1} to {score2}</p>
-      <p>Use Buttons Below to Keep Score</p>
-
+      {!winner && <p>Use Buttons Below to Keep Score</p>}
+      {winner && <h1 style={{color:'green'}}>{winner} Won</h1>}
       <label>Select the score:</label>
       <select value={goal} onChange={(e) => setGoal(e.target.value)}>
         <option value="">Choose...</option>
@@ -43,8 +44,8 @@ function App() {
         <option value="10">10</option>
       </select>
 
-      <button onClick={handleClick1}>+1 player1</button>
-      <button onClick={handleClick2}>+1 player2</button>
+      <button onClick={handleClick1} disabled={winner||!goal}>+1 player1</button>
+      <button onClick={handleClick2} disabled={winner||!goal}>+1 player2</button>
       <button onClick={reset}>Reset</button>
     </>
   )
